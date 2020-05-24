dfa80a195523906db949f2c8dc0d671ad26b4a43
Theng <wai.loon.theng@accenture.com>
2020-05-24 10:17:42 +0800

    Initial commit

---
+++ src/main/kotlin/com/example/dinozords/DinozordsApplication.kt
@@ -0,0 +1,11 @@
+package com.example.dinozords
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DinozordsApplication
+
+fun main(args: Array<String>) {
+	runApplication<DinozordsApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/dinozords/DinozordsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.dinozords
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DinozordsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
